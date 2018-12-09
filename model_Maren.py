395c91c38eeda4d30b78b8630c52bf3832c52b48
Maahrii <marenbuermann@unitybox.de>
2018-12-09 18:03:03 +0100

    Update model_Maren.py

--- model_Maren.py
+++ model_Maren.py
@@ -33,18 +33,18 @@ def model_fn(labels_dim):
     """Create a Keras Sequential model with layers."""
 
     model = models.Sequential()
-    model.add(Conv2D(filters=32, kernel_size=(3, 3), strides=(2,2)))
-	model.add(LeakyReLU(alpha=0.3))
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), strides=(2,2), input_shape=(128, 128, 3)))
+	model.add(LeakyReLU())
 	model.add(MaxPooling2D(pool_size=(3, 3), stride=(2,2)))
     model.add(Conv2D(filters=64, kernel_size=(3, 3)))
-	model.add(LeakyReLU(alpha=0.3))
+	model.add(LeakyReLU())
     model.add(MaxPooling2D(pool_size=(3, 3), stride=(2,2)))
 	model.add(Conv2D(filters=128, kernel_size=(3,3)))
-	model.add(LeakyReLU(alpha=0.3))
+	model.add(LeakyReLU())
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(128))
-	model.add(LeakyReLU(alpha=0.3))
+	model.add(LeakyReLU())
     model.add(Dropout(0.5))
     model.add(Dense(labels_dim, activation='softmax'))
 
