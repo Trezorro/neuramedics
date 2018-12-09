8331ee87706989708a6e0d1c218cca7e75ddb1ff
Maahrii <marenbuermann@unitybox.de>
2018-12-09 20:26:37 +0100

    Update model_Maren.py

--- model_Maren.py
+++ model_Maren.py
@@ -34,13 +34,15 @@ def model_fn(labels_dim):
 
     model = models.Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),
-                     activation='relu',
-                     input_shape=(128, 128, 3)))
-    model.add(Conv2D(64, (3, 3), strides=(2, 2), activation='relu'))
+                     input_shape=(128, 128, 3))) 
+	model.add(LeakyReLU(alpha=0.1))
+    model.add(Conv2D(64, (3, 3), strides=(2, 2)))
+	model.add(LeakyReLU(alpha=0.1))
     model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
     model.add(Dropout(0.25))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
+    model.add(Dense(128))
+	model.add(LeakyReLU(alpha=0.1))
     model.add(Dropout(0.25))
     model.add(Dense(labels_dim, activation='softmax'))
 
