6aa794158d1e94aa98e783304ae6eea65c1028b7
Maahrii <marenbuermann@unitybox.de>
2018-12-09 21:34:49 +0100

    Update model_Maren.py

--- model_Maren.py
+++ model_Maren.py
@@ -33,19 +33,17 @@ def model_fn(labels_dim):
     """Create a Keras Sequential model with layers."""
 
     model = models.Sequential()
-    model.add(Conv2D(64, kernel_size=(3, 3),
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), strides=(2,2)
                      activation='relu',
-                     input_shape=(128, 128, 3)))
-    model.add(Conv2D(64, (7, 7), strides=(2, 2), activation='relu'))
-	 model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3)))
-	 model.add(Conv2D(64, (3, 3), activation='relu'))
-	 model.add(Conv2D(64, (3, 3), activation='relu'))
-	 model.add(MaxPooling2D(pool_size=(3, 3)))
+                     input_shape=(256, 256, 3)))
+	model.add(MaxPooling2D(pool_size=(3, 3), stride=(2,2)))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3, 3), stride=(2,2)))
+	model.add(Conv2D(filters=128, kernel_size=(3,3), activation='relu'))
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
     model.add(Dense(labels_dim, activation='softmax'))
 
     compile_model(model)
