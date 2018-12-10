a2bbac7ae3e97b25e761d9faf0a25af9f40bfcdd
Maahrii <marenbuermann@unitybox.de>
2018-12-10 17:50:16 +0100

    Update model_Maren.py

--- model_Maren.py
+++ model_Maren.py
@@ -36,21 +36,49 @@ def model_fn(labels_dim):
     model.add(Conv2D(64, kernel_size=(3, 3),
                      activation='relu',
                      input_shape=(128, 128, 3)))
-    model.add(Conv2D(64, (7, 7), strides=(2, 2), activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (7, 7), strides=(2, 2)))
+    model.add(LeakyReLU(alpha = 0.3))
     model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
     model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.25))
     model.add(Dense(labels_dim, activation='softmax'))
     compile_model(model)
-    return model
-
-
+    return model	
+	
 def compile_model(model):
     model.compile(loss=keras.losses.categorical_crossentropy,
                   optimizer='Adam',
