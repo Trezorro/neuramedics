a072d6cceb466a911f21ccf20d24d2ef792b6187
Maahrii <marenbuermann@unitybox.de>
2018-12-14 13:07:21 +0100

    Update model_Maren.py

--- model_Maren.py
+++ model_Maren.py
@@ -44,6 +44,9 @@ def model_fn(labels_dim):
     model.add(Conv2D(64, (3, 3)))
     model.add(LeakyReLU(alpha = 0.3))
     model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
     model.add(Dropout(0.4))
     model.add(Flatten())
     model.add(Dense(128))
@@ -51,7 +54,7 @@ def model_fn(labels_dim):
     model.add(Dropout(0.4))
     model.add(Dense(labels_dim, activation='softmax'))
     compile_model(model)
-    return model	
+    return model
 	
 
 def compile_model(model):
