657a947ac75ec342a0f8b2a4d6155f6b51d2d260
Milan <milan.tresoor@gmail.com>
2018-12-17 15:20:13 +0100

    Got the model prediction output working and tested it on a few models

---
+++ diagnose.py
@@ -0,0 +1,45 @@
+import keras
+import keras.preprocessing.image as image
+import numpy as np
+import os
+import base64
+import plotly
+import plotly.graph_objs as go
+
+import matplotlib.pyplot as plt
+
+SHOWGRAPH = False
+model = keras.models.load_model('retinopathy4.hdf5')
+
+x =[]
+
+for file in os.listdir("./data/sample"):
+    img = image.load_img('data/sample/'+file, target_size=(128, 128, 3))
+    plt.imshow(img)
+    plt.show()
+    img = image.img_to_array(img)
+    x = np.resize(img, (1, 128,128,3))
+    prediction = model.predict(x)
+    print("Image {:<15}:    {}\n".format(file,prediction))
+
+
+
+    if SHOWGRAPH:
+        encoded_image = base64.b64encode(open("./data/sample/"+file,'rb').read())
+
+        plotly.offline.plot({
+        "data": [go.Bar(x=["Healthy","Stage 1","Stage 2","Stage 3","Stage 4"], y=prediction[0])],
+        "layout": go.Layout(title="Prediction",
+                            images= [dict(
+                  source= 'data:image/jpg;base64,{}'.format(encoded_image.decode()),
+                  xref= "0",
+                  yref= "0",
+                  x= 1,
+                  y= 1,
+                  sizex= 0,
+                  sizey= 0,
+                  sizing= "contain",
+                  opacity= 0.8,
+                  layer= "above")])
+        }, auto_open=True)
+        break
