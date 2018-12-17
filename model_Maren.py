c3ac154063e40294858642c9e222530575cb8d1b
Maahrii <marenbuermann@unitybox.de>
2018-12-17 15:49:59 +0100

    Update model_Maren.py

--- model_Maren.py
+++ model_Maren.py
@@ -48,12 +48,10 @@ def model_fn(labels_dim):
     model.add(Conv2D(64, (5, 5)))
     model.add(LeakyReLU(alpha = 0.3))
     model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-    model.add(BatchNormalization())
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(128))
     model.add(LeakyReLU(alpha = 0.3))
-    model.add(BatchNormalization())
     model.add(Dropout(0.25))
     model.add(Dense(labels_dim, activation='softmax'))
     compile_model(model)
@@ -63,7 +61,7 @@ def model_fn(labels_dim):
 
 def compile_model(model):
     model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer='Adam',
+                  optimizer='Adamax',
                   metrics=['accuracy'])
     return model
 
