71cfc176290cfcd7b2dea11820455563eb6d457c
Maahrii <marenbuermann@unitybox.de>
2018-12-28 14:33:14 +0100

    Update model_Maren.py

--- model_Maren.py
+++ model_Maren.py
@@ -78,6 +78,40 @@ def read_train_data():
     return [X_train, Y_train]
 
 
+def createTernaryY(one_hot_labels): 
+    lst = [0 if i[0] == 1 else 1 if (i[1] == 1 or i[2]==1) else 2 for i in one_hot_labels] 
+    return keras.utils.to_categorical(lst, num_classes=3) 
+
+def take_balanced(CLASS_SIZE, X, Y, sample_num):
+    sample_indices = {}
+    counter = 0
+    for i in range(0, CLASS_SIZE): 
+    # initialize a dictionary with list of indices as values 
+		 sample_indices = {}
+    while (not all(len(value) == sample_num for value in sample_indices.values())) and (counter != (len(Y))):
+		 class_to_put = int(np.where([Y[counter] == 1])[1]) #establishes the class of the instance 
+		 if len(sample_indices['class{0}'.format(class_to_put)]) < sample_num:
+			 sample_indices['class{0}'.format(class_to_put)].append(counter)
+		 counter +=1 
+    indices = [item for sublist in sample_indices.values() for item in sublist]
+    X_sub = np.array([X[k] for k in indices]) 
+    Y_sub = np.array([Y[z] for z in indices]) 
+    return X_sub, Y_sub 
+	
+
+trinary_Y = createTernaryY(Y_test) 
+trinary_Y[:3]
+
+
+a, b = take_balanced(3, X_test, trinary_Y , 6)
+
+
+a.shape 
+b
+
+
+
+
 def read_test_data():
     start_time = time.time()
     print("Start Read Test Data")
