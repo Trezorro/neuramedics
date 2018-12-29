ddc3e2ec75fb78c8b04c954ceaaa412942064e62
blazejmanczak <manczakb@gmail.com>
2018-12-29 08:36:52 +0100

    Prepping for maren's model on ternary

--- model.py
+++ model.py
@@ -51,15 +51,25 @@ def model_fn(labels_dim):
     """Create a Keras Sequential model with layers."""
 
     model = models.Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),
+    model.add(Conv2D(64, kernel_size=(3, 3),
                      activation='relu',
                      input_shape=(128, 128, 3)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Conv2D(64, (7, 7), strides=(2, 2)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Dropout(0.4))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha = 0.3))
+    model.add(Dropout(0.4))
     model.add(Dense(labels_dim, activation='softmax'))
     compile_model(model)
 
