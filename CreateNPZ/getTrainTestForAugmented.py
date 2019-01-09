4ee5e1058374ce397ee6a416c9145a3ead486bd9
blazejmanczak <manczakb@gmail.com>
2019-01-09 19:29:09 +0100

    Adapted model to run on preprocess fixed.

--- CreateNPZ/getTrainTestForAugmented.py
+++ CreateNPZ/getTrainTestForAugmented.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import os
 
-all_labels = pd.read_csv("/Users/blazejmanczak/Desktop/School/Year2/Q2/DataChallange1/augmented_labels.csv", usecols=['image', 'level'])
+all_labels = pd.read_csv("/Users/blazejmanczak/Desktop/School/Year2/Q2/DataChallange1/labels_augmented_fixed.csv", usecols=['image', 'level'])
 original_labels = pd.read_csv("/Users/blazejmanczak/Desktop/School/Year2/Q2/DataChallange1/trainLabels.csv")
+
+all_labels = all_labels.sample(frac =1, random_state = 1998)
 def convert_ternary(row):
     if (row == 1) | (row == 2):
         return 1
@@ -25,14 +27,14 @@ def hyphen_split(a):
         return "_".join(a.split("_", 2)[:2])
 
 uniqe_ids = set(all_labels['image'].apply(hyphen_split))
-
+all_labels[:1]
 def get_train_test(size_train, size_test, original_labels):
     ## We are bounded by the size of class 3
-    train0 = original_labels[original_labels['level']==0][(size_test*3): (size_test+size_train)*3]['image'] # *3 because in augmented we double healthy and 6x sick
+    train0 = original_labels[original_labels['level']==0][(size_test*3)+1: (size_test+size_train)*3+1]['image'] # *3 because in augmented we double healthy and 6x sick
     train1 = original_labels[original_labels['level']==1][size_test: size_test+size_train]['image']
     train2 = original_labels[original_labels['level']==2][size_test: size_test+size_train]['image']
 
-    test0 = original_labels[original_labels['level']==0][:(size_test*3)]['image']
+    test0 = original_labels[original_labels['level']==0][:(size_test*3)+1]['image']
     test1 = original_labels[original_labels['level']==1][:size_test]['image']
     test2 = original_labels[original_labels['level']==2][:size_test]['image']
 
@@ -49,14 +51,13 @@ test_images_csv = all_labels[all_labels['image'].str.match('|'.join(test_images)
 train_images_csv.groupby(by=['level']).aggregate("count")
 test_images_csv.groupby(by=['level']).aggregate("count")
 
-train_images_csv.to_csv("train_images_augment.csv")
-test_images_csv.to_csv("test_images_augment.csv")
-
+train_images_csv.to_csv("train_images_augment_fixed.csv")
+test_images_csv.to_csv("test_images_augment_fixed.csv")
 
 ### Checking the independence
 
-train_images = pd.read_csv("/Users/blazejmanczak/Desktop/School/Year2/Q2/DataChallange1/train_images_augment.csv", usecols=['image', 'level'])
-test_images = pd.read_csv("/Users/blazejmanczak/Desktop/School/Year2/Q2/DataChallange1/test_images_augment.csv", usecols=['image', 'level'])
+train_images = pd.read_csv("/Users/blazejmanczak/Desktop/School/Year2/Q2/DataChallange1/train_images_augment_fixed.csv", usecols=['image', 'level'])
+test_images = pd.read_csv("/Users/blazejmanczak/Desktop/School/Year2/Q2/DataChallange1/test_images_augment_fixed.csv", usecols=['image', 'level'])
 
 uniqe_ids_train = set(train_images['image'].apply(hyphen_split))
 uniqe_ids_test = set(test_images['image'].apply(hyphen_split))
